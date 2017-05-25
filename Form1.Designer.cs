c42fca63363d8ffd44d7ca6974231cf35dfc3d2f
yetju000 <yetju000steam@gmail.com>
2017-05-25 17:17:14 +0200

    Gotowy Client UDP

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.haslo = new System.Windows.Forms.TextBox();
+            this.Info = new System.Windows.Forms.TextBox();
+            this.zaloguj = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.TextBox();
+            this.Zarejestruj = new System.Windows.Forms.Button();
+            this.Wyloguj = new System.Windows.Forms.Button();
+            this.Pliki = new System.Windows.Forms.TextBox();
+            this.sciezka_Wrzuc = new System.Windows.Forms.TextBox();
+            this.Plik = new System.Windows.Forms.TextBox();
+            this.sciezkaZapis = new System.Windows.Forms.TextBox();
+            this.Wrzuc = new System.Windows.Forms.Button();
+            this.Sciagnij = new System.Windows.Forms.Button();
+            this.nazwaPlikuWrzut = new System.Windows.Forms.TextBox();
+            this.odswiez = new System.Windows.Forms.Button();
+            this.usun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // haslo
+            // 
+            this.haslo.AcceptsTab = true;
+            this.haslo.Location = new System.Drawing.Point(89, 66);
+            this.haslo.Name = "haslo";
+            this.haslo.Size = new System.Drawing.Size(100, 20);
+            this.haslo.TabIndex = 1;
+            this.haslo.Text = "haslo";
+            this.haslo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.haslo.TextChanged += new System.EventHandler(this.haslo_TextChanged);
+            // 
+            // Info
+            // 
+            this.Info.Location = new System.Drawing.Point(36, 162);
+            this.Info.Name = "Info";
+            this.Info.ReadOnly = true;
+            this.Info.Size = new System.Drawing.Size(225, 20);
+            this.Info.TabIndex = 3;
+            this.Info.TextChanged += new System.EventHandler(this.Info_TextChanged);
+            // 
+            // zaloguj
+            // 
+            this.zaloguj.Location = new System.Drawing.Point(36, 108);
+            this.zaloguj.Name = "zaloguj";
+            this.zaloguj.Size = new System.Drawing.Size(100, 35);
+            this.zaloguj.TabIndex = 4;
+            this.zaloguj.Text = "Zaloguj";
+            this.zaloguj.UseVisualStyleBackColor = true;
+            this.zaloguj.Click += new System.EventHandler(this.zaloguj_Click);
+            // 
+            // login
+            // 
+            this.login.AcceptsTab = true;
+            this.login.Location = new System.Drawing.Point(89, 29);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 5;
+            this.login.Text = "login";
+            this.login.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.login.TextChanged += new System.EventHandler(this.login_TextChanged);
+            // 
+            // Zarejestruj
+            // 
+            this.Zarejestruj.Location = new System.Drawing.Point(163, 108);
+            this.Zarejestruj.Name = "Zarejestruj";
+            this.Zarejestruj.Size = new System.Drawing.Size(98, 35);
+            this.Zarejestruj.TabIndex = 6;
+            this.Zarejestruj.Text = "Zarejestruj";
+            this.Zarejestruj.UseVisualStyleBackColor = true;
+            this.Zarejestruj.Click += new System.EventHandler(this.Zarejestruj_Click);
+            // 
+            // Wyloguj
+            // 
+            this.Wyloguj.Location = new System.Drawing.Point(77, 108);
+            this.Wyloguj.Name = "Wyloguj";
+            this.Wyloguj.Size = new System.Drawing.Size(127, 35);
+            this.Wyloguj.TabIndex = 7;
+            this.Wyloguj.Text = "Wyloguj";
+            this.Wyloguj.UseVisualStyleBackColor = true;
+            this.Wyloguj.Visible = false;
+            this.Wyloguj.Click += new System.EventHandler(this.Wyloguj_Click);
+            // 
+            // Pliki
+            // 
+            this.Pliki.Location = new System.Drawing.Point(570, 29);
+            this.Pliki.Multiline = true;
+            this.Pliki.Name = "Pliki";
+            this.Pliki.ReadOnly = true;
+            this.Pliki.Size = new System.Drawing.Size(255, 438);
+            this.Pliki.TabIndex = 8;
+            this.Pliki.Visible = false;
+            this.Pliki.TextChanged += new System.EventHandler(this.Pliki_TextChanged);
+            // 
+            // sciezka_Wrzuc
+            // 
+            this.sciezka_Wrzuc.Location = new System.Drawing.Point(36, 230);
+            this.sciezka_Wrzuc.Name = "sciezka_Wrzuc";
+            this.sciezka_Wrzuc.Size = new System.Drawing.Size(369, 20);
+            this.sciezka_Wrzuc.TabIndex = 9;
+            this.sciezka_Wrzuc.Text = "sciezka pliku do wrzucenia na server";
+            this.sciezka_Wrzuc.Visible = false;
+            // 
+            // Plik
+            // 
+            this.Plik.Location = new System.Drawing.Point(36, 350);
+            this.Plik.Name = "Plik";
+            this.Plik.Size = new System.Drawing.Size(369, 20);
+            this.Plik.TabIndex = 10;
+            this.Plik.Text = "Ktory plik";
+            this.Plik.Visible = false;
+            // 
+            // sciezkaZapis
+            // 
+            this.sciezkaZapis.Location = new System.Drawing.Point(36, 376);
+            this.sciezkaZapis.Name = "sciezkaZapis";
+            this.sciezkaZapis.Size = new System.Drawing.Size(369, 20);
+            this.sciezkaZapis.TabIndex = 11;
+            this.sciezkaZapis.Text = "sciezka gdzie zapisac";
+            this.sciezkaZapis.Visible = false;
+            // 
+            // Wrzuc
+            // 
+            this.Wrzuc.Location = new System.Drawing.Point(135, 256);
+            this.Wrzuc.Name = "Wrzuc";
+            this.Wrzuc.Size = new System.Drawing.Size(126, 44);
+            this.Wrzuc.TabIndex = 12;
+            this.Wrzuc.Text = "Wrzuc";
+            this.Wrzuc.UseVisualStyleBackColor = true;
+            this.Wrzuc.Visible = false;
+            this.Wrzuc.Click += new System.EventHandler(this.Wrzuc_Click);
+            // 
+            // Sciagnij
+            // 
+            this.Sciagnij.Location = new System.Drawing.Point(135, 402);
+            this.Sciagnij.Name = "Sciagnij";
+            this.Sciagnij.Size = new System.Drawing.Size(126, 46);
+            this.Sciagnij.TabIndex = 13;
+            this.Sciagnij.Text = "Sciagnij";
+            this.Sciagnij.UseVisualStyleBackColor = true;
+            this.Sciagnij.Visible = false;
+            this.Sciagnij.Click += new System.EventHandler(this.Sciagnij_Click);
+            // 
+            // nazwaPlikuWrzut
+            // 
+            this.nazwaPlikuWrzut.Location = new System.Drawing.Point(36, 204);
+            this.nazwaPlikuWrzut.Name = "nazwaPlikuWrzut";
+            this.nazwaPlikuWrzut.Size = new System.Drawing.Size(369, 20);
+            this.nazwaPlikuWrzut.TabIndex = 14;
+            this.nazwaPlikuWrzut.Text = "nazwa pliku wrzucanego/usuwanego z servera";
+            this.nazwaPlikuWrzut.Visible = false;
+            // 
+            // odswiez
+            // 
+            this.odswiez.Location = new System.Drawing.Point(489, 30);
+            this.odswiez.Name = "odswiez";
+            this.odswiez.Size = new System.Drawing.Size(75, 56);
+            this.odswiez.TabIndex = 15;
+            this.odswiez.Text = "odswiez liste plikow";
+            this.odswiez.UseVisualStyleBackColor = true;
+            this.odswiez.Visible = false;
+            this.odswiez.Click += new System.EventHandler(this.odswiez_Click);
+            // 
+            // usun
+            // 
+            this.usun.Location = new System.Drawing.Point(330, 152);
+            this.usun.Name = "usun";
+            this.usun.Size = new System.Drawing.Size(75, 46);
+            this.usun.TabIndex = 17;
+            this.usun.Text = "usun plik";
+            this.usun.UseVisualStyleBackColor = true;
+            this.usun.Visible = false;
+            this.usun.Click += new System.EventHandler(this.usun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 504);
+            this.Controls.Add(this.usun);
+            this.Controls.Add(this.odswiez);
+            this.Controls.Add(this.nazwaPlikuWrzut);
+            this.Controls.Add(this.Sciagnij);
+            this.Controls.Add(this.Wrzuc);
+            this.Controls.Add(this.sciezkaZapis);
+            this.Controls.Add(this.Plik);
+            this.Controls.Add(this.sciezka_Wrzuc);
+            this.Controls.Add(this.Pliki);
+            this.Controls.Add(this.Wyloguj);
+            this.Controls.Add(this.Zarejestruj);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.zaloguj);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.haslo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox haslo;
+        private System.Windows.Forms.TextBox Info;
+        private System.Windows.Forms.Button zaloguj;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.Button Zarejestruj;
+        private System.Windows.Forms.Button Wyloguj;
+        private System.Windows.Forms.TextBox Pliki;
+        private System.Windows.Forms.TextBox sciezka_Wrzuc;
+        private System.Windows.Forms.TextBox Plik;
+        private System.Windows.Forms.TextBox sciezkaZapis;
+        private System.Windows.Forms.Button Wrzuc;
+        private System.Windows.Forms.Button Sciagnij;
+        private System.Windows.Forms.TextBox nazwaPlikuWrzut;
+        private System.Windows.Forms.Button odswiez;
+        private System.Windows.Forms.Button usun;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        String adres_IP = "192.168.43.91";
+        byte[] ServerLoginByte = new byte[20];
+        byte[] ServerHasloByte = new byte[20];
+        UdpClient client;   
+        string path;
+        IPEndPoint ep;
+        public Form1()
+        {
+            client = new UdpClient();
+            ep = new IPEndPoint(IPAddress.Parse(adres_IP), 4444); // inicjujemy klienta
+            client.Connect(ep);
+            InitializeComponent();
+            client.Send(new byte[] { 15 }, 1);
+        }
+        private void zaloguj_Click(object sender, EventArgs e)
+        {
+            path = "/home/adrian/Pulpit/Katalog/" + login.Text;
+            string wiad;
+            byte[] LoginByte = new byte[login.Text.Length];
+            byte[] HasloByte = new byte[haslo.Text.Length];
+            LoginByte = System.Text.Encoding.UTF8.GetBytes(login.Text);
+            HasloByte = System.Text.Encoding.UTF8.GetBytes(haslo.Text);
+
+            client.Send(new byte[] { 48 }, 1);  //wysylamy ze niezalogowany
+            client.Send(new byte[] { 49 }, 1);   //chcemy sie logowac
+            client.Send(LoginByte, LoginByte.Length); //dane logowania
+            client.Send(HasloByte, HasloByte.Length);  //dane logowania
+
+            byte[] wiadomosc = new byte[30];
+            wiadomosc = client.Receive(ref ep);
+            wiad = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+
+            if (wiad.Contains("zalogowano pomyslnie")) {
+                ServerLoginByte = LoginByte;
+                ServerHasloByte = HasloByte;
+                
+                znikaj();
+                Pliki.Text = "";
+                while(true)
+                {
+                    wiadomosc = client.Receive(ref ep);
+                    wiad = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+                    if (wiad.Contains("q")) break;
+                    Pliki.Text = Pliki.Text + wiad;
+                    Pliki.AppendText(Environment.NewLine);
+                }
+                Info.Text = "zalogowano pomyslnie";
+            }
+            else
+            {
+                path = "";
+                Info.Text = wiad;
+            }
+            wiad = null;
+            LoginByte = null;
+            HasloByte = null;
+            wiadomosc = null;
+
+        }
+       
+
+        private void login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Zarejestruj_Click(object sender, EventArgs e)
+        {
+            byte[] LoginByte = new byte[login.Text.Length];
+            byte[] HasloByte = new byte[haslo.Text.Length];
+            LoginByte = System.Text.Encoding.UTF8.GetBytes(login.Text);
+            HasloByte = System.Text.Encoding.UTF8.GetBytes(haslo.Text);
+
+            client.Send(new byte[] { 48 }, 1); //wysylamy ze nie zalogowany
+            client.Send(new byte[] { 50 }, 1);  //wysylamy ze rejestracja
+            client.Send(LoginByte, LoginByte.Length);
+            client.Send(HasloByte, HasloByte.Length);
+
+            byte[] wiadomosc = new byte[30];
+            wiadomosc = client.Receive(ref ep);
+            
+            Info.Text = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+
+            LoginByte = null;
+            HasloByte = null;
+            wiadomosc = null;
+        }
+
+        private void Wyloguj_Click(object sender, EventArgs e)
+        {
+            
+            client.Send(new byte[] { 57}, 1);
+            client.Send(ServerLoginByte, ServerLoginByte.Length);
+            client.Send(ServerHasloByte, ServerHasloByte.Length);
+            client.Send(new byte[] { 49 }, 1);
+          
+            pojaw();
+            Info.Text = "Wylogowano";
+        }
+
+
+
+        private void Wrzuc_Click(object sender, EventArgs e)
+        {
+         
+
+            client.Send(new byte[] { 57 }, 1); //wysylamy ze zalogowany
+            client.Send(ServerLoginByte, ServerLoginByte.Length);
+            client.Send(ServerHasloByte, ServerHasloByte.Length);
+            client.Send(new byte[] { 51 }, 1); //wysylamy ze chcemy wrzucic
+            client.Send(System.Text.Encoding.UTF8.GetBytes(path), path.Length); //wysylamy gdzie
+
+            byte[] nazwaByte = new byte[nazwaPlikuWrzut.Text.Length];
+            nazwaByte = System.Text.Encoding.UTF8.GetBytes(nazwaPlikuWrzut.Text);
+            client.Send(nazwaByte, nazwaByte.Length);  //wysylamy nazwe do utworzenia
+            
+
+
+            Pliki.Text = "";
+            byte[] wiadomosc2 = new byte[100];
+            wiadomosc2 = client.Receive(ref ep);
+            string wiad = System.Text.Encoding.UTF8.GetString(wiadomosc2);
+            if (wiad.Contains("."))
+            {
+                Info.Text = "brak wolnych portow";
+                Info.Refresh();
+            }
+            else {
+                UdpClient client2;   //57 logged , 48 not logged
+
+                IPEndPoint ep2;
+                client2 = new UdpClient();
+                ep2 = new IPEndPoint(IPAddress.Parse(adres_IP), Convert.ToInt32(wiad)); // endpoint where server is listening
+                client2.Connect(ep2);
+
+                int gg = 0;
+                int ilepakietow = 0;
+                string wyslij = "";
+                using (var reader = File.OpenText(@sciezka_Wrzuc.Text))
+                {
+                    string pomoc;
+                    while ((pomoc = reader.ReadLine()) != null)
+                    {
+
+                        ilepakietow++;
+                    }
+                }
+                StreamReader sr = new StreamReader(sciezka_Wrzuc.Text);
+                wyslij = "";
+                while (sr.Peek() >= 0)
+                {
+                    string zmienna = sr.ReadLine();
+                    
+                   
+                    //C:\Users\Adrian\Desktop\plik.txt
+                    gg++;
+                    Info.Text = "wyslanych pakietow : " + Convert.ToString(gg) + "/" + Convert.ToString(ilepakietow);
+                    Info.Refresh();
+                    
+                    
+                        string dlugosc = Convert.ToString(zmienna.Length);
+                        client2.Send(System.Text.Encoding.UTF8.GetBytes(dlugosc), dlugosc.Length);
+
+                        client2.Send(System.Text.Encoding.UTF8.GetBytes(zmienna), zmienna.Length); //wysylamy pakiety
+                        wyslij = "";
+                   
+
+                }
+
+                if (wyslij.Length != 0)
+                {
+                    string dlugosc = Convert.ToString(wyslij.Length);
+                    client2.Send(System.Text.Encoding.UTF8.GetBytes(dlugosc), dlugosc.Length);
+
+                    client2.Send(System.Text.Encoding.UTF8.GetBytes(wyslij), wyslij.Length); //wysylamy pakiety
+
+                }
+
+                client2.Send(new byte[] { 49 }, 1); //wysylamy ze koniec odbioru
+                client2.Send(new byte[] { 113 }, 1); //wysylamy ze koniec odbioru
+
+                sr.Close();
+                
+
+                //  string wiad;
+                    client.Send(new byte[] { 50 }, 1);
+                while (true)
+                {
+                    byte[] wiadomosc = new byte[100];
+                    wiadomosc = client2.Receive(ref ep);
+                    wiad = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+                    if (wiad.Contains("q")) break;
+                    Pliki.Text = Pliki.Text + wiad;
+                    Pliki.AppendText(Environment.NewLine);
+                }
+                Info.Text = "plik zostal umieszczony na serverze";
+                Info.Refresh();
+                client2.Close();
+            }
+        }
+
+        private void Sciagnij_Click(object sender, EventArgs e)
+        {
+            
+                string wiad;
+            byte[] wiadomosc = new byte[512];
+            byte[] plik = new byte[path.Length+Plik.Text.Length+1];
+            plik = System.Text.Encoding.UTF8.GetBytes(path+'/'+Plik.Text);
+            
+            client.Send(new byte[] { 57 }, 1); //wysylamy ze zalogowany
+            client.Send(ServerLoginByte, ServerLoginByte.Length);
+            client.Send(ServerHasloByte, ServerHasloByte.Length);
+            client.Send(new byte[] { 52 }, 1); //wysylamy ze chcemy sciagnac
+            client.Send(plik, plik.Length);
+
+            byte[] wiadomosc2 = new byte[100];
+            wiadomosc2 = client.Receive(ref ep);
+            string wiad2 = System.Text.Encoding.UTF8.GetString(wiadomosc2);
+            
+            if (wiad2.Contains("."))
+            {
+                Info.Text = "brak wolnych portow";
+                Info.Refresh();
+            }
+            else {
+                UdpClient client2;   //57 logged , 48 not logged
+
+                IPEndPoint ep2;
+                client2 = new UdpClient();
+                
+                ep2 = new IPEndPoint(IPAddress.Parse(adres_IP), Convert.ToInt32(wiad2)); // endpoint where server is listening
+
+                client2.Connect(ep2);
+               
+                client2.Send(new byte[] { 1 }, 1);
+                wiadomosc = client2.Receive(ref ep2);
+                
+                wiad = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+
+
+
+                if (wiad[0] == '.')
+                {
+                    Info.Text = "";
+                    Info.Text = "nie ma takiego pliku";
+                    Info.Refresh();
+
+
+                }
+                else
+                {
+                    FileStream fs = File.Create(sciezkaZapis.Text);
+                    
+                    while (true)
+                    {
+
+                        fs.Write(wiadomosc, 0, wiadomosc.Length);
+                        fs.Write(System.Text.Encoding.UTF8.GetBytes(System.Environment.NewLine), 0, Environment.NewLine.Length);
+                        wiadomosc = client2.Receive(ref ep2);
+
+                        if (wiadomosc[0] == '.') break;
+                    }
+                    fs.Close();
+                }
+                Info.Text = "sciaganie zakonczone pomyslnie";
+                Info.Refresh();
+
+            }
+        }
+
+        private void odswiez_Click(object sender, EventArgs e)
+        {
+            string wiad;
+            byte[] wiadomosc = new byte[512];
+            byte[] sciezka = new byte[path.Length];
+            sciezka = System.Text.Encoding.UTF8.GetBytes(path);
+            
+            client.Send(new byte[] { 57 }, 1); //wysylamy ze zalogowany
+            client.Send(ServerLoginByte, ServerLoginByte.Length);
+            client.Send(ServerHasloByte, ServerHasloByte.Length);
+            client.Send(new byte[] { 50 }, 1); //wysylamy ze chcemy wrzucic
+            client.Send(sciezka, sciezka.Length); //path
+            Pliki.Text = "";
+            
+            while (true)
+            {
+                wiadomosc = client.Receive(ref ep);
+                wiad = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+                if (wiad.Contains("q")) break;
+                Pliki.Text = Pliki.Text + wiad;
+                Pliki.AppendText(Environment.NewLine);
+             
+            }
+            
+        }
+
+        private void usun_Click(object sender, EventArgs e)
+        {
+            bool a = true;
+            string pomoc = nazwaPlikuWrzut.Text;
+            for (int i=0; i<pomoc.Length; i++)
+            {
+                if (pomoc[i] == ' ')
+                {
+                    a = false;
+                    break;
+                }
+            }
+            if (a == true)
+            {
+                string wiad = path + "/" + pomoc;
+
+                byte[] wiadomosc = new byte[512];
+                byte[] sciezka = new byte[wiad.Length];
+                sciezka = System.Text.Encoding.UTF8.GetBytes(wiad);
+                
+                client.Send(new byte[] { 57 }, 1); //wysylamy ze zalogowany
+                client.Send(ServerLoginByte, ServerLoginByte.Length);
+                client.Send(ServerHasloByte, ServerHasloByte.Length);
+                client.Send(new byte[] { 53 }, 1); //wysylamy ze chcemy usunac
+
+                client.Send(sciezka, sciezka.Length); //path 
+                wiadomosc = client.Receive(ref ep);
+                wiad = System.Text.Encoding.UTF8.GetString(wiadomosc, 0, wiadomosc.Length);
+                Info.Text = wiad;
+                Info.Refresh();
+            
+            }
+            else
+            {
+                Info.Text = "";
+                Info.Text = "nazwa nie moze miec spacji";
+                Info.Refresh();
+            }
+        }
+
+        public void znikaj()
+        {
+            haslo.Visible = false;
+            login.Visible = false;
+            zaloguj.Visible = false;
+            Zarejestruj.Visible = false;
+            Wyloguj.Visible = true;
+            sciezka_Wrzuc.Visible = true;
+            sciezkaZapis.Visible = true;
+            Plik.Visible = true;
+            Sciagnij.Visible = true;
+            Wrzuc.Visible = true;
+            nazwaPlikuWrzut.Visible = true;
+            odswiez.Visible = true;
+            Pliki.Visible = true;
+            usun.Visible = true;
+        }
+                                            //pojaw() i znikaj() do obslugi interfaceu
+        public void pojaw()
+        {
+            haslo.Visible = true;
+            login.Visible = true;
+            zaloguj.Visible = true;
+            Zarejestruj.Visible = true;
+            Wyloguj.Visible = false;
+            Pliki.Visible = false;
+            sciezka_Wrzuc.Visible = false;
+            sciezkaZapis.Visible = false;
+            Plik.Visible = false;
+            Sciagnij.Visible = false;
+            Wrzuc.Visible = false;
+            nazwaPlikuWrzut.Visible = false;
+            odswiez.Visible = false;
+            usun.Visible = false;
+            Pliki.Text = "";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Info_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void haslo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Pliki_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
